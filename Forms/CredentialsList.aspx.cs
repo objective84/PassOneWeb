c68849d88b63dfa810cb47001beebde2f9823e03
phowland1 <phowland1@gmail.com>
2013-03-17 01:01:22 -0400

    Initial Push

---
+++ Forms/CredentialsList.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PassOne.Forms
+{
+    public partial class CredentialsList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Edit_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("EditValue.aspx");
+        }
+    }
+}
